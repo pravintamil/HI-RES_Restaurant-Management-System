9163aa547f6701f3b58acd1c8cb283f17265d1e3
Balamurugan12345 <balamurugan@knonex.com>
2016-05-31 10:22:14 +0530

    sd

--- .fr-UYAVi4/HI-RES/dashboard/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config['callback_url']         =   'http://localhost/testing/hotapp/dashboard/fbfeed.php?fbTrue=true'; //    /?fbTrue=true allow you to code process section.
-
-//Facebook configuration
-$config['App_ID']      =   '1681879898744901';
-$config['App_Secret']  =   '94387049d624f6196e4b308094f99452'; 
-
-?>
--- .fr-sUTJmK/HI-RES/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$con=mysql_connect("127.0.0.1","root","pravin_1");
-	mysql_select_db("hi-res_restaurant");
-	$tz=mysql_query("SET time_zone='+05:30'");
-?>
